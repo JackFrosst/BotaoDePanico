8846ea8fed34f167c014a42049407335760edb92
JackFrosst <matheusrodriguesamancio@hotmail.com>
2021-09-27 22:02:08 -0300

    add numero de telefone automatico

--- app/src/main/java/botaopanico/principal/TelaCadastro.java
+++ app/src/main/java/botaopanico/principal/TelaCadastro.java
@@ -1,37 +1,36 @@
 package botaopanico.principal;
 
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
-import android.Manifest;
-import android.content.Context;
+import android.app.PendingIntent;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
+import android.content.IntentSender;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.telephony.TelephonyManager;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class TelaCadastro extends AppCompatActivity {
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.credentials.Credential;
+import com.google.android.gms.auth.api.credentials.CredentialPickerConfig;
+import com.google.android.gms.auth.api.credentials.HintRequest;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class TelaCadastro extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
 
     private EditText edtNome, edtSobrenome, edtTelefone;
     Button btnEnviar, btnVerificaNumero;
     RementeDestinatario rementeDestinatario = new RementeDestinatario();
 
-    private final int PERMISSAO_REQUEST = 1;
+    private GoogleApiClient googleApiClient_telefone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,38 +41,6 @@ public class TelaCadastro extends AppCompatActivity {
         edtSobrenome = findViewById(R.id.edtSobrenome);
         edtTelefone = findViewById(R.id.edtNumTelefone);
         btnEnviar = findViewById(R.id.btnEnviar);
-        btnVerificaNumero = findViewById(R.id.btnVerificaNumero);
-
-        if (ActivityCompat.checkSelfPermission(TelaCadastro.this, Manifest.permission.READ_SMS) !=
-                PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(TelaCadastro.this, Manifest.permission.READ_PHONE_NUMBERS) !=
-                PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(TelaCadastro.this, Manifest.permission.READ_PHONE_STATE) !=
-                PackageManager.PERMISSION_GRANTED) {
-
-            if (ActivityCompat.shouldShowRequestPermissionRationale(TelaCadastro.this,
-                    Manifest.permission.READ_PHONE_STATE) ||
-                    ActivityCompat.shouldShowRequestPermissionRationale(TelaCadastro.this, Manifest.permission.READ_PHONE_NUMBERS) ||
-                    ActivityCompat.shouldShowRequestPermissionRationale(TelaCadastro.this, Manifest.permission.READ_SMS)) {
-
-            } else {
-                ActivityCompat.requestPermissions(TelaCadastro.this, new String[]{
-                        Manifest.permission.READ_PHONE_STATE,
-                        Manifest.permission.READ_PHONE_NUMBERS,
-                        Manifest.permission.READ_SMS}, PERMISSAO_REQUEST);
-            }
-        }
-
-        TelephonyManager telephonyManager = (TelephonyManager) this.getSystemService(Context.TELEPHONY_SERVICE);
-        String number = telephonyManager.getLine1Number();
-        Toast.makeText(this, "Número: " + number, Toast.LENGTH_LONG).show();
-
-
-        btnVerificaNumero.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-
-        });
 
 
         btnEnviar.setOnClickListener(new View.OnClickListener() {
@@ -97,25 +64,84 @@ public class TelaCadastro extends AppCompatActivity {
 
             }
         });
+
+        googleApiClient_telefone = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this).enableAutoManage(this,this)
+                .addApi(Auth.CREDENTIALS_API).build();
+
+        obterNumeroContato();
+
+    }
+
+    //----------------------- METODOS DO GOOGLE API PARA CONSEGUIR O NÚMERO DO USUÁRIO ---------------------------------
+
+    private void obterNumeroContato(){
+        HintRequest hintRequest = new HintRequest.Builder()
+                .setHintPickerConfig(new CredentialPickerConfig.Builder().setShowCancelButton(false).build())
+                .setPhoneNumberIdentifierSupported(true).build();
+
+        PendingIntent intent = Auth.CredentialsApi.getHintPickerIntent(googleApiClient_telefone,hintRequest);
+        try {
+            startIntentSenderForResult(intent.getIntentSender(),123,null,0,0,0);
+        } catch (IntentSender.SendIntentException e) {
+            e.printStackTrace();
+        }
+
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
-        switch (requestCode) {
-            case PERMISSAO_REQUEST:
-                if (grantResults.length > 0 &&
-                        grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+        if(requestCode == 123){
 
+            if(resultCode == RESULT_OK){
+                Credential credential = data.getParcelableExtra(Credential.EXTRA_KEY);
 
-                }  else {
-                    // Explain to the user that the feature is unavailable because
-                    // the features requires a permission that the user has denied.
-                    // At the same time, respect the user's decision. Don't link to
-                    // system settings in an effort to convince the user to change
-                    // their decision.
-                }
+                    if(!credential.getId().isEmpty()){
+                        edtTelefone.setText(credential.getId());
+                    }else{
+                        Toast.makeText(TelaCadastro.this, "Escolha um número de contato",
+                                Toast.LENGTH_SHORT).show();
+                    }
+            }else{
+                caixaDialogAlerta();
+            }
         }
+    }
+
+    private void caixaDialogAlerta(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(TelaCadastro.this)
+                .setTitle("Escolha obrigatória")
+                .setCancelable(false)
+                .setMessage("Escolha um número de telefone para cadastro!")
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        obterNumeroContato();
+                    }
+                }).setNegativeButton("Sair", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                });
+
+        builder.show();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
