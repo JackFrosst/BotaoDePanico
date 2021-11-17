b1529b3674e00169bb658b448aef0d4ceecc62e3
MrRobot3 <72826577+MrRobot3@users.noreply.github.com>
2021-11-17 17:25:27 -0300

    adição toolbar tela duvida

--- app/src/main/java/botaopanico/principal/Duvida.java
+++ app/src/main/java/botaopanico/principal/Duvida.java
@@ -1,14 +1,21 @@
 package botaopanico.principal;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class Duvida extends AppCompatActivity {
-
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_duvida);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setTitle("Dúvidas");
     }
 }
--- app/src/main/java/botaopanico/principal/Sobre.java
+++ app/src/main/java/botaopanico/principal/Sobre.java
@@ -1,8 +1,10 @@
 package botaopanico.principal;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class Sobre extends AppCompatActivity {
 
@@ -10,5 +12,6 @@ public class Sobre extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sobre);
+
     }
 }
